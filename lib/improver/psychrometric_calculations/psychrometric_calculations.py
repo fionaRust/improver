635ec975a0df0cffe1e8205759e5c1d1603cb7c6
fiona.rust <fiona.rust@metoffice.gov.uk>
2018-07-02 14:20:28 +0100

    Adjusting filled in values based on orography at filled in points

--- lib/improver/psychrometric_calculations/psychrometric_calculations.py
+++ lib/improver/psychrometric_calculations/psychrometric_calculations.py
@@ -909,7 +909,7 @@ class FallingSnowLevel(object):
                                              sea_points)
 
     @staticmethod
-    def fill_in_by_horizontal_interpolation(snow_level_data):
+    def fill_in_by_horizontal_interpolation(snow_level_data, orog_data):
         """
         Fill in any remaining unset areas in the snow falling level by using
         linear horizontal intepolation across the grid.
@@ -930,6 +930,8 @@ class FallingSnowLevel(object):
         (y_points, x_points) = np.mgrid[0:ynum, 0:xnum]
         snow_level_data_updated = griddata(
             points, values, (y_points, x_points), method='linear')
+        snow_level_above_orog = ~np.isfinite(snow_level_data) & (snow_level_data_updated > orog_data)
+        snow_level_data_updated[snow_level_above_orog] = 0.0 #orog_data[snow_level_above_orog]
         return snow_level_data_updated
 
     def process(self, temperature, relative_humidity, pressure, orog,
@@ -1001,7 +1003,7 @@ class FallingSnowLevel(object):
                 snow_cube.data, land_sea_data, wb_integral.data.max(axis=0),
                 wet_bulb_temp.data,  heights)
             snow_cube.data = self.fill_in_by_horizontal_interpolation(
-                snow_cube.data)
+                snow_cube.data, orog_data)
             # Fill in any remaining points with missing data:
             remaining_points = np.where(np.isnan(snow_cube.data))
             snow_cube.data[remaining_points] = self.missing_data
