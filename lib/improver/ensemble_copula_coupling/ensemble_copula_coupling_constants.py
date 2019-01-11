2095e1f1f5a33fdadef5c9733437f6881eef3c47
fiona.rust <fiona.rust@metoffice.gov.uk>
2019-01-11 16:35:00 +0000

    Extended the ECC bounds to cover range of thresholds

--- lib/improver/ensemble_copula_coupling/ensemble_copula_coupling_constants.py
+++ lib/improver/ensemble_copula_coupling/ensemble_copula_coupling_constants.py
@@ -53,7 +53,7 @@ BOUNDS_FOR_ECDF = {
     "air_temperature": (
         Bounds((-140-ABSOLUTE_ZERO, 60-ABSOLUTE_ZERO), "Kelvin")),
     "air_potential_temperature": (
-        Bounds((-20-ABSOLUTE_ZERO, 30-ABSOLUTE_ZERO), "Kelvin")),
+        Bounds((-20-ABSOLUTE_ZERO, 32-ABSOLUTE_ZERO), "Kelvin")),
     "wind_speed": Bounds((0, 50), "m s^-1"),
     "wind_speed_of_gust": Bounds((0, 350), "m s^-1"),
     "air_pressure_at_sea_level": Bounds((94000, 107000), "Pa"),
